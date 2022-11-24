9e50a7ea6558e29d92fc11260500ec1500af8c7f
marcosmatioli <100784085+marcosmatioli@users.noreply.github.com>
2022-11-24 10:42:26 -0300

    Att Projeto Veterinaria V3

--- ProjetoVeterinaria/Models/modelVeterinario.cs
+++ ProjetoVeterinaria/Models/modelVeterinario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,10 @@ namespace ProjetoVeterinaria.Models
 {
     public class modelVeterinario
     {
+        [Required(ErrorMessage = "Informe o codigo do veterinario")]
         [DisplayName("Codigo do Veterinario")]
         public int codVet { get; set; }
+        [Required(ErrorMessage = "Informe o nome do veterinario")]
         [DisplayName("Nome do Veterinario")]
         public string nomeVet { get; set; }
     }
--- bdClinicaVeterinaria.sql
+++ bdClinicaVeterinaria.sql
@@ -56,14 +56,14 @@ constraint foreign key(codVet) references tbVeterinario(codVet)
 #drop table tbAtendimento;
 #drop table tbAgendamento;
 #selects
-select * from tbCliente;
+#select * from tbCliente;
 #select * from tbAtendimento;
 #select * from tbAnimal;
 #select * from tbTipoAnimal;
 #select * from tbVeterinario;
 #select * from tbAgendamento;
 
-#Criação das views
+#Criação das views para o consultar grid
 create view vwAtendimento as select * from tbAtendimento;
 create view vwAgendamento as select * from tbAgendamento;
 create view vwCliente as select * from tbCliente;
@@ -80,11 +80,11 @@ select usuario,senha,tipoUsuario from tbCliente where usuario = "adm" and senha 
 select * from tbCliente;
 #inserir tipo de animal
 insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (1,"Roedor");
-insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (2,"Passaro");
+insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (2,"Papagaio");
 insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (3,"Peixe");
 insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (4,"Gato");
 insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (5,"Cachorro");
-insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (6,"Outros");
+insert into tbTipoAnimal(codTipoAnimal,TipoAnimal) values (6,"Piriquito");
 
 #inserir veterinario
 insert into tbVeterinario(codVet,nomeVet) values (1,"Alex Karev");
@@ -98,31 +98,30 @@ insert into tbVeterinario(codVet,nomeVet) values (7,"Mark Sloan");
 #select para aparecer os animais do usuario logado, consegui fazer o inner join que traz os animais do 
 #usuario em questão agora só alinhar os dados que eu quero
 #trazer para usar no ASP, mudar no where para colocar qual o usuario em questão que vai mostrar os animais dele
-select tbAnimal.codAnimal,tbAnimal.nomeAnimal,tbAnimal.fotoAnimal,tbAnimal.codTipoAnimal,tbAnimal.codCliente,
-tbCliente.nomeCliente from tbAnimal inner join tbCliente 
-on tbAnimal.codCliente = tbCliente.codCliente where tbCliente.nomeCliente = "ADM";
-
-#select para trazer o codigo do cliente para usar no inner join acima pois não tem uma variavel no programa inteiro que fica disponivel, para usar de comparação
-select tbCliente.codCliente from tbCliente where usuario = "adm" and senha = "adm";
+-- select tbAnimal.codAnimal,tbAnimal.nomeAnimal,tbAnimal.fotoAnimal,tbAnimal.codTipoAnimal,tbAnimal.codCliente,tbCliente.nomeCliente from tbAnimal inner join tbCliente on tbAnimal.codCliente = tbCliente.codCliente where tbCliente.nomeCliente = "ADM";
 
 #update no cliente, teste para ver se iria funcionar
-update tbCliente set nomeCliente="user",telefoneCliente="88 88888-8888",cepCliente="55555-555",enderecoCliente="Rua Dronsfiled numero 30",usuario="usuario",senha="usuario",TipoUsuario=2 where codCliente=2;
+-- update tbCliente set nomeCliente="user",telefoneCliente="88 88888-8888",cepCliente="55555-555",enderecoCliente="Rua Dronsfiled numero 30",usuario="usuario",senha="usuario",TipoUsuario=2 where codCliente=2;
 
 #select da tabela agendamento
-select * from tbAgendamento;
+-- select * from tbAgendamento;
 
 #select para trazer todos os agendamentos de um cliente com base no codCliente
-select tbAgendamento.codAgendamento,tbAgendamento.dataAgendamento,tbAgendamento.horaAgendamento,tbAgendamento.Reclamacao,tbVeterinario.nomeVet,tbAgendamento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAgendamento
-inner join tbAnimal on tbAnimal.codAnimal = tbAgendamento.codAnimal inner join tbVeterinario on tbAgendamento.codVet = tbVeterinario.codVet inner join tbCliente on tbCliente.codCliente = tbAgendamento.codCliente where tbAgendamento.codCliente = 1;
+-- select tbAgendamento.codAgendamento,tbAgendamento.dataAgendamento,tbAgendamento.horaAgendamento,tbAgendamento.Reclamacao,tbVeterinario.nomeVet,tbAgendamento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAgendamento
+-- inner join tbAnimal on tbAnimal.codAnimal = tbAgendamento.codAnimal inner join tbVeterinario on tbAgendamento.codVet = tbVeterinario.codVet inner join tbCliente on tbCliente.codCliente = tbAgendamento.codCliente where tbAgendamento.codCliente = 1;
 
 #select que traz todos os agendamentos
-select tbAgendamento.codAgendamento,tbAgendamento.dataAgendamento,tbAgendamento.horaAgendamento,tbAgendamento.Reclamacao,tbVeterinario.nomeVet,tbAgendamento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAgendamento  
-inner join tbAnimal on tbAnimal.codAnimal = tbAgendamento.codAnimal inner join tbVeterinario on tbAgendamento.codVet = tbVeterinario.codVet inner join tbCliente on tbAgendamento.codCliente = tbCliente.codCliente;
+-- select tbAgendamento.codAgendamento,tbAgendamento.dataAgendamento,tbAgendamento.horaAgendamento,tbAgendamento.Reclamacao,tbVeterinario.nomeVet,tbAgendamento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAgendamento  
+-- inner join tbAnimal on tbAnimal.codAnimal = tbAgendamento.codAnimal inner join tbVeterinario on tbAgendamento.codVet = tbVeterinario.codVet inner join tbCliente on tbAgendamento.codCliente = tbCliente.codCliente;
 
 #select para trazer todos os atendimentos de um cliente com base no codCliente
-select tbAtendimento.codAtendimento,tbAtendimento.dataAtendimento,tbAtendimento.horaAtendimento,tbAtendimento.Diagnostico,tbVeterinario.nomeVet,tbAtendimento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAtendimento  
-inner join tbAnimal on tbAnimal.codAnimal = tbAtendimento.codAnimal inner join tbVeterinario on tbAtendimento.codVet = tbVeterinario.codVet inner join tbCliente on tbAtendimento.codCliente = tbCliente.codCliente where tbAtendimento.codCliente = 1;
+-- select tbAtendimento.codAtendimento,tbAtendimento.dataAtendimento,tbAtendimento.horaAtendimento,tbAtendimento.Diagnostico,tbVeterinario.nomeVet,tbAtendimento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAtendimento inner join tbAnimal on tbAnimal.codAnimal = tbAtendimento.codAnimal inner join tbVeterinario on tbAtendimento.codVet = tbVeterinario.codVet inner join tbCliente on tbAtendimento.codCliente = tbCliente.codCliente where tbAtendimento.codCliente = 1;
 
 #select que traz todos os atendimentos
-select tbAtendimento.codAtendimento,tbAtendimento.dataAtendimento,tbAtendimento.horaAtendimento,tbAtendimento.Diagnostico,tbVeterinario.nomeVet,tbAtendimento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAtendimento  
-inner join tbAnimal on tbAnimal.codAnimal = tbAtendimento.codAnimal inner join tbVeterinario on tbAtendimento.codVet = tbVeterinario.codVet inner join tbCliente on tbAtendimento.codCliente = tbCliente.codCliente;
+-- select tbAtendimento.codAtendimento,tbAtendimento.dataAtendimento,tbAtendimento.horaAtendimento,tbAtendimento.Diagnostico,tbVeterinario.nomeVet,tbAtendimento.codVet,tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAtendimento inner join tbAnimal on tbAnimal.codAnimal = tbAtendimento.codAnimal inner join tbVeterinario on tbAtendimento.codVet = tbVeterinario.codVet inner join tbCliente on tbAtendimento.codCliente = tbCliente.codCliente;
+
+#select para trazer animal e nome cliente
+-- select tbAnimal.nomeAnimal,tbCliente.nomeCliente from tbAnimal inner join tbCliente on tbAnimal.codAnimal = tbCliente.codCliente;
+
+#teste concat para trazer em uma tabela o nome do usuario e o pet junto e tinha que ter uma tabela separada do codigo do animal ( isso aqui demorou umas 3 horinhas humilde pra entender e fazer )
+-- select tbAnimal.codAnimal,CONCAT('Dono: ',nomeCliente,' | Pet: ', nomeAnimal) from tbCliente inner join tbAnimal on tbAnimal.codCliente = tbCliente.codCliente;
